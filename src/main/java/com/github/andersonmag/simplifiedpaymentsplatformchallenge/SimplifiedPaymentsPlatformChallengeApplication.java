c24fab00a1514d16d622985683a4e3bd85686730
Anderson <andersondel.dev@gmail.com>
2025-03-31 20:38:08 -0300

    chore: initial project

---
+++ src/main/java/com/github/andersonmag/simplifiedpaymentsplatformchallenge/SimplifiedPaymentsPlatformChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.github.andersonmag.simplifiedpaymentsplatformchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentsPlatformChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplifiedPaymentsPlatformChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/andersonmag/simplifiedpaymentsplatformchallenge/SimplifiedPaymentsPlatformChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andersonmag.simplifiedpaymentsplatformchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentsPlatformChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
